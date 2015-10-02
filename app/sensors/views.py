f2cca9b9defe30aa23ada8cac2dc8ea6cbe652e8
ChristopherGS <christopher.samiullah@gmail.com>
2015-10-02 09:14:26 +0800

    initial csv2db save working

--- app/sensors/views.py
+++ app/sensors/views.py
@@ -1,13 +1,15 @@
-import os
+import os, csv
+from datetime import datetime
+from numpy import genfromtxt
 
 from flask import abort, Blueprint, flash, jsonify, Markup, redirect, render_template, request, \
-url_for
+url_for, session
 from random import choice
 from flask.ext.login import current_user, login_required
 
 from .forms import SiteForm, VisitForm
-from .models import Site, Visit
-from app.data import query_to_list
+from .models import Site, Visit, Sensor
+from app.data import query_to_list, db
 
 from werkzeug import secure_filename
 
@@ -22,6 +24,10 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+def Load_Data(file_name):
+    data = genfromtxt(file_name, delimiter=',', skiprows=1, converters={0: lambda s: str(s)})
+    return data.tolist()
+
 
 
 @sensors.route("/")
@@ -34,7 +40,7 @@ def index():
 
 
 @sensors.route('/csv', methods=['GET', 'POST'])
-def csv():
+def csv_route():
     if request.method == 'GET':
         return render_template('sensors/csv.html')
     elif request.method == 'POST':
@@ -42,8 +48,53 @@ def csv():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+            # need to open the CSV and iterate row by row
+            date = datetime.now()
+
+            try:
+                print filename
+                file_name = filename
+                data = Load_Data(os.path.join(UPLOAD_FOLDER, file_name)) 
+                for i in data:
+                    record = Sensor(**{
+                        'accelerometer_x' : i[1],
+                        'accelerometer_y' : i[2],
+                        'accelerometer_z' : i[3],
+                    })
+                    db.session.add(record) #Add all the records
+
+                db.session.commit() #Attempt to commit all the records
+            except Exception as e:
+                print e
+                # db.session.rollback() #Rollback the changes on error
+            finally:
+                pass
+                # db.session.close() #Close the connection
+            
+            """
+            input_reader = csv.reader('sensor_log.csv', delimiter=',', quotechar="'", quoting=csv.QUOTE_ALL)
+            count = 0
+            holder = [] 
+            for line in input_reader:
+                blah = line[0]
+                obj = Sensor(line[0])
+                holder.append(blah)
+                db.session.add(obj)
+                count += 1
+            
+            db.session.commit()
+            print count
+            print holder
+            """
+
+            # new_stamp = Sensor(accelerometer_x=request.form["ownerName"], accelerometer_y=request.form["roomSize"]\
+            #           , accelerometer_z=request.form["roomDescription"], timestamp=request.form["roomPrice"])
+            
+
             # TODO save to DB function
             # Test change for Urs
+            flash("CSV data saved to DB") 
             return redirect(url_for('sensors.complete')), 201
         else:
             return render_template('index.html'), 400
