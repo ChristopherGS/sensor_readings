231e91b95446b1c9ca890fe263647664a4757bb5
ChristopherGS <christopher.samiullah@gmail.com>
2016-01-17 17:45:02 +0800

    more broken LSTM tests

--- ML_Sandbox/rnn.py
+++ ML_Sandbox/rnn.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Dropout, Activation, TimeDistributedDense
 from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import LSTM
+from keras.layers.recurrent import LSTM, GRU
 from keras.datasets import cifar10
 from sklearn import cross_validation
 
@@ -34,6 +34,8 @@ print y_train.shape
 
 #======================================================
 
+"""
+
 model = Sequential()
 model.add(Embedding(3, 16, input_length=3))
 model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
@@ -46,9 +48,79 @@ model.compile(loss='binary_crossentropy', optimizer='rmsprop')
 X = np.column_stack([x1, x2, x3])
 Y = np.column_stack([y_train])
 
-model.fit(X, Y, batch_size=16, nb_epoch=10)
-print model.predict(np.column_stack([_x1, _x2, _x3]))
-print np.stack(y_test)
+model.fit(X, Y, batch_size=16, nb_epoch=5)
+
+score, acc = model.evaluate(np.column_stack([_x1, _x2, _x3]), np.column_stack([y_test]),show_accuracy=True)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+classification = model.predict_classes(np.column_stack([_x1, _x2, _x3]), verbose=1)
+
+print classification
+"""
+
+
+#=========================================================
+"""
+print('Build model 2...')
+model2 = Sequential()
+model2.add(Embedding(3, 128, input_length=100))
+model2.add(LSTM(128))  # try using a GRU instead, for fun
+model2.add(Dropout(0.5))
+model2.add(Dense(1))
+model2.add(Activation('sigmoid'))
+
+# try using different optimizers and different optimizer configs
+model2.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              class_mode="binary")
+
+print("Train...")
+
+X = np.column_stack([x1, x2, x3])
+Y = np.column_stack([y_train])
+
+_X = np.column_stack([_x1, _x2, _x3])
+_Y = np.column_stack([y_test])
+
+model2.fit(X, Y, batch_size=32, nb_epoch=5,
+          validation_data=(_X, _Y), show_accuracy=True)
+score, acc = model2.evaluate(_X, _Y,
+                            batch_size=32,
+                            show_accuracy=True)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+"""
+
+#====================================================
+
+"""
+NOT SURE ABOUT TIMESTEPS
+
+https://github.com/bigaidream/subsets_ml_cookbook/blob/d4e9e8def2068c83390257d0b5aed9072bf4ece6/dl/theano/theano_keras_sequence2sequence.md
+
+n_in_out = 3
+n_hidden = 100
+n_samples = 2297
+n_timesteps = 400
+
+X = [len(X_train), n_timesteps, np.column_stack([x1, x2, x3])]
+Y = np.column_stack([y_train])
+
+_X = np.column_stack([_x1, _x2, _x3])
+_Y = np.column_stack([y_test])
+
+model3 = Sequential()
+
+model3.add(GRU( n_hidden, input_dim = n_in_out, return_sequences=True))
+model3.add(TimeDistributedDense(n_in_out, input_dim = n_hidden))
+model3.compile(loss='mse', optimizer='rmsprop')
+
+X_final = np.random.random((n_samples, n_timesteps, n_in))
+Y_final = np.random.random((n_samples, n_timesteps, n_out))
+
+model.fit(X, Y, nb_epoch=10, validation_data=(_X, _Y), show_accuracy=True)
 
 
 """
@@ -89,4 +161,6 @@ Just your regular fully connected NN layer.
 
 7) One epoch consists of one full training cycle on the training set. 
 Once every sample in the set is seen, you start again - marking the beginning of the 2nd epoch.
+
+8) To discuss: Should try GRU instead of LSTM
 """
